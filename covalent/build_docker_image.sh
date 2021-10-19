466a03fdffc3f43c6a17cb523c7d83e61d76b48b
miiu96 <mihai9625@gmail.com>
2021-10-19 11:27:04 +0300

    fixes after review

--- covalent/build_docker_image.sh
+++ covalent/build_docker_image.sh
@@ -4,4 +4,4 @@ CONFIG_TAG="D1.2.24.0-covalent1.0.3"
 # network can we "mainnet", "testnet" or "devnet"
 NETWORK="devnet"
 
-docker build --build-arg CONFIG_TAG=${CONFIG_TAG} --build-arg NETWORK=${NETWORK} . -t ${IMAGE_NAME} -f ./elrond-node-obs
+docker build --build-arg CONFIG_TAG=${CONFIG_TAG} --build-arg NETWORK=${NETWORK} . -t ${IMAGE_NAME} -f ./observer/Dockerfile
