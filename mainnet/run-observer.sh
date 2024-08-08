eda3b74b930086dd582360393be3fa2add314d72
miiu <mihai9625@gmail.com>
2024-08-08 10:16:46 +0300

    upgrade squad

--- mainnet/run-observer.sh
+++ mainnet/run-observer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run --mount type=bind,source=${OBSERVER_DIR}/db,destination=/go/mx-chain-go/cmd/node/db --mount type=bind,source=${OBSERVER_DIR}/logs,destination=/go/mx-chain-go/cmd/node/logs --mount type=bind,source=${OBSERVER_DIR}/config,destination=/config --publish ${P2P_PORT}:37373 --network=multiversx-squad --ip=${IP} --name squad-${SHARD} multiversx/chain-observer:v1.7.13.0 \
+docker run --mount type=bind,source=${OBSERVER_DIR}/db,destination=/go/mx-chain-go/cmd/node/db --mount type=bind,source=${OBSERVER_DIR}/logs,destination=/go/mx-chain-go/cmd/node/logs --mount type=bind,source=${OBSERVER_DIR}/config,destination=/config --publish ${P2P_PORT}:37373 --network=multiversx-squad --ip=${IP} --name squad-${SHARD} multiversx/chain-observer:v1.7.13.0-patch1 \
 --destination-shard-as-observer=${SHARD} --validator-key-pem-file=/config/observerKey_${SHARD}.pem --display-name="${DISPLAY_NAME}"
 
