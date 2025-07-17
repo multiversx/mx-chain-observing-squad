d29109ae4a41c04016446d323255213716ffa67d
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2025-07-17 14:38:42 +0300

    Release v1.10.4.0

--- mainnet/run-proxy.sh
+++ mainnet/run-proxy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- docker run --network=multiversx-squad --ip=${IP} --name proxy multiversx/chain-squad-proxy:v1.2.0
+ docker run --network=multiversx-squad --ip=${IP} --name proxy multiversx/chain-squad-proxy:v1.3.0
