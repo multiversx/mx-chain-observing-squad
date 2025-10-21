9176710a0f98a7f20ba808fedaf2ad96fc855bfe
miiu <mihai9625@gmail.com>
2025-10-21 11:04:44 +0300

    upgrade squad

--- mainnet/run-proxy.sh
+++ mainnet/run-proxy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- docker run --network=multiversx-squad --ip=${IP} --name proxy multiversx/chain-squad-proxy:v1.3.2
+ docker run --network=multiversx-squad --ip=${IP} --name proxy multiversx/chain-squad-proxy:v1.3.3
